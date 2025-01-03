54de995f1df5cb4a8d75edb020b80a4cf4277a3b
theparadox7 <theunknownparadox07@gmail.com>
2025-01-03 19:26:49 +0530

    Allmost done with frontend

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,49 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [result, setResult] = useState("")
-  const handleClick = e => setResult(result.concat(e.target.id))
-  const clear =() => setResult("")
-  const deleteEL  = () => setResult(result.slice((0,-1)))
+  const [result, setResult] = useState('');
+
+  const handleClick = (e) => setResult(result.concat(e.target.id));
+  const clear = () => setResult('');
+  const deleteEL = () => setResult(result.slice(0, -1));
 
   const calculate = () => {
-    try{
-      setResult(eval((result),toString()))
-    }catch(error){
-      setResult("Error")
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult('Error');
     }
-  }
+  };
 
   return (
-
-     <div className="calculator-grid">
+    <div className="calculator-grid">
       <div className="output">
-          <div className="prev-operand"></div>
-          <div className="current-operand"></div>
+        <div className="previous-operand"></div>
+        <div className="current-operand">{result}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>*</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="span-two">=</button>
+      <button className="span-two orange" onClick={clear}>
+        AC
+      </button>
+      <button className="orange" onClick={deleteEL}>
+        DEL
+      </button>
+      <button id="/" onClick={handleClick}>
+        /
+      </button>
+      <button id="1" onClick={handleClick}>
+        1
+      </button>
+      <button id="2" onClick={handleClick}>
+        2
+      </button>
+      <button id="3" onClick={handleClick}>
+        3
+      </button>
+      <button id="*" onClick={handleClick}>
+        *
+      </button>
+      <button id="4" onClick={handleClick}>
+        4
+      </button>
+      <button id="5" onClick={handleClick}>
+        5
+      </button>
+      <button id="6" onClick={handleClick}>
+        6
+      </button>
+      <button id="+" onClick={handleClick}>
+        +
+      </button>
+      <button id="7" onClick={handleClick}>
+        7
+      </button>
+      <button id="8" onClick={handleClick}>
+        8
+      </button>
+      <button id="9" onClick={handleClick}>
+        9
+      </button>
+      <button id="-" onClick={handleClick}>
+        -
+      </button>
+      <button id="." onClick={handleClick}>
+        .
+      </button>
+      <button id="0" className="orange" onClick={handleClick}>
+        0
+      </button>
+      <button className="span-two orange" onClick={calculate}>
+        =
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
