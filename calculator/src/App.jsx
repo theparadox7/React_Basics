1d01e9c55522983e31313a576ef45127157e17fd
theparadox7 <theunknownparadox07@gmail.com>
2025-01-03 19:03:58 +0530

    simplified and building calculator

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState("")
+  const handleClick = e => setResult(result.concat(e.target.id))
+  const clear =() => setResult("")
+  const deleteEL  = () => setResult(result.slice((0,-1)))
+
+  const calculate = () => {
+    try{
+      setResult(eval((result),toString()))
+    }catch(error){
+      setResult("Error")
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+     <div className="calculator-grid">
+      <div className="output">
+          <div className="prev-operand"></div>
+          <div className="current-operand"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>*</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
   )
 }
 
